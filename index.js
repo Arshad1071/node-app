888dfa390bdcb473d22ee8dcee8cfea95ce085db
Arshad vp <Arshad1071@yahoo.in>
2024-02-07 16:11:11 +0400

    react app app added

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`listing on port ${PORT}`);
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.get("/api/test", (req, res) => {
+  res.send({
+    name: "Arshad",
+  });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/test", (req, res) => {
-  res.send("Hello ");
-});
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
